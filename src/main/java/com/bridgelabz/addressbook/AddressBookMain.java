62197f63c4bdfe01fafaae6f224c641a0c9c9e34
prem-12345 <premkpatil1997@gmail.com>
2021-09-10 09:47:27 +0530

    Welcome to address book collection program

---
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+
+        // Welcome to Address book collection program
+
+    }
+
+}
+
