7d4f6be9a92c9b3363048eafa47ceeabb29e84fb
prem-12345 <premkpatil1997@gmail.com>
2021-09-11 15:00:16 +0530

    Added ability to create new contact in addressbook

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,13 +1,40 @@
 package com.bridgelabz.addressbook;
 
-
+import java.util.Scanner;
 public class AddressBookMain {
 
     public static void main(String[] args) {
 
       //Welcome to Address Book Collection program
 
+        getInput();
+
     }
 
+    //UC2 ABILIY TO ADD NEW CONTACT TO THE ADDRESS BOOK
+
+    public static void getInput() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("ENTER THE FIRST NAME: ");
+        String firstName = input.nextLine();
+        System.out.print("ENTER THE LAST NAME: ");
+        String lastName = input.nextLine();
+        System.out.print("ENTER THE ADDRESS: ");
+        String address = input.nextLine();
+        System.out.print("ENTER THE CITY: ");
+        String city = input.nextLine();
+        System.out.print("ENTER THE STATE: ");
+        String state = input.nextLine();
+        System.out.print("ENTER THE EMAIL ID: ");
+        String email = input.nextLine();
+        System.out.print("ENTER THE ZIPCODE: ");
+        Long zipcode = input.nextLong();
+        System.out.print("ENTER THE PHONE NUMBER: ");
+        Long phoneNumber = input.nextLong();
+
+        Contact person = new Contact(firstName, lastName, address, city, state, email, zipcode, phoneNumber);
+        person.display();
+    }
 
   }
